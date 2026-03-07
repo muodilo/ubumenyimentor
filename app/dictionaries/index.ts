8695aec3b6a6bb8536c20369404fde24737ae1d7
muodilo <odilomurindahabi@gmail.com>
2026-03-07 17:31:58 +0200

    ft/ implement language selection

---
+++ app/dictionaries/index.ts
@@ -0,0 +1,15 @@
+import "server-only";
+
+const dictionaries = {
+  en: () => import("./en.json").then((module) => module.default),
+  fr: () => import("./fr.json").then((module) => module.default),
+  rw: () => import("./rw.json").then((module) => module.default),
+};
+
+export type Locale = keyof typeof dictionaries;
+
+export const hasLocale = (locale: string): locale is Locale =>
+  locale in dictionaries;
+
+export const getDictionary = async (locale: Locale) => dictionaries[locale]();
+
